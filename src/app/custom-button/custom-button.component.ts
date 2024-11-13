18ae1d98de41c93d09d7e3209333b011a030b437
Drix <narcisodrix@gmail.com>
2024-11-13 22:07:28 +0800

    added router link

--- src/app/custom-button/custom-button.component.ts
+++ src/app/custom-button/custom-button.component.ts
@@ -1,18 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';  // Import IonicModule for IonButton
 import { CommonModule } from '@angular/common'; // Import CommonModule for ngClass
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-custom-button',
   templateUrl: './custom-button.component.html',
   styleUrls: ['./custom-button.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule]  
+  imports: [IonicModule, CommonModule, RouterModule] 
 })
 export class CustomButtonComponent implements OnInit {
 
-  @Input() text: string = 'Button';  
-  @Input() customClass: string = ''; 
+  @Input() text: string = 'Button';       
+  @Input() customClass: string = '';    
+  @Input() routerLink: string = '';       
 
   constructor() {}
 
