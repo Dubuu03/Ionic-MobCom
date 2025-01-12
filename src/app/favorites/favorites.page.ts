e1f0795cf8e38704965cf3a8aeeca51ae9490f2f
Drix <narcisodrix@gmail.com>
2025-01-13 01:37:34 +0800

    added favorites page

--- src/app/favorites/favorites.page.ts
+++ src/app/favorites/favorites.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface FavoriteItem {
+  name: string;
+  category: string;
+  rating: number;
+  image: string;
+  isFavorite: boolean;
+}
+
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.page.html',
@@ -7,9 +15,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesPage implements OnInit {
 
+  favorites: FavoriteItem[] = [
+    {
+      name: 'Lavender Green Chill',
+      category: 'Non-coffee',
+      rating: 5.0,
+      image: 'assets/images/Non-Coffee/Lavander Green Chill.PNG',
+      isFavorite: true
+    },
+    {
+      name: 'Vanilla Bean Brew',
+      category: 'Cold coffee',
+      rating: 5.0,
+      image: 'assets/images/Cold/Vanilla Bean Brew.PNG',
+      isFavorite: true
+    },
+    {
+      name: 'Berry Cool Milkshake',
+      category: 'Non-coffee',
+      rating: 5.0,
+      image: 'assets/images/Non-Coffee/Berry Cool Milkshake.PNG',
+      isFavorite: true
+    },
+    {
+      name: 'Strawberry Blush Frost',
+      category: 'Non-coffee',
+      rating: 5.0,
+      image: 'assets/images/Non-Coffee/Strawberry Blush Frost.PNG',
+      isFavorite: true
+    },
+    {
+      name: 'Herbal Frost',
+      category: 'Cold coffee',
+      rating: 5.0,
+      image: 'assets/images/Cold/Herbal Frost.PNG',
+      isFavorite: true
+    },
+    {
+      name: 'CinnaDeja Delight',
+      category: 'Pastry',
+      rating: 5.0,
+      image: 'assets/images/Pastry/CinnaDeja Delight.PNG',
+      isFavorite: true
+    }
+  ];
+
+  clearAll() {
+    this.favorites = [];
+  }
+
+  toggleFavorite(item: FavoriteItem) {
+    item.isFavorite = !item.isFavorite;
+    this.favorites = this.favorites.filter(favorite => favorite.isFavorite);
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  goToHome() {
+    window.location.href = '/home';
+  }
+  goToNotifications() {
+    window.location.href = '/notifications';
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,7 +9,280 @@ export class HomePage implements OnInit {
 
   products: Array<any> = [];
   
-  slidesPerView: number = 1.1;  // Default value for larger screens
+  slidesPerView: number = 2.2;  // Default value for larger screens
+
+  bestSellerProducts = [
+    {
+      rating: 4.5,
+      title: 'Strawberry Blush Frost',
+      price: 120,
+      productImageUrl: 'assets/images/Non-Coffee/Strawberry Blush Frost.PNG',
+      favorite: false,
+      productId: 123,
+      productPath: '/product-details/123'
+    },
+    {
+      rating: 4.3,
+      title: 'Lavender Green Chill',
+      price: 125,
+      productImageUrl: 'assets/images/Non-Coffee/Lavander Green Chill.PNG',
+      favorite: false,
+      productId: 124,
+      productPath: '/product-details/124'
+    },
+    {
+      rating: 4.5,
+      title: 'Choco Deja Puff',
+      price: 135,
+      productImageUrl: 'assets/images/Pastry/Choco Deja Puff.PNG',
+      favorite: false,
+      productId: 125,
+      productPath: '/product-details/125'
+    },
+    {
+      rating: 4.4,
+      title: 'Caramel Kiss',
+      price: 100,
+      productImageUrl: 'assets/images/Hot/Coco-caramel Kiss.PNG',
+      favorite: false,
+      productId: 126,
+      productPath: '/product-details/126'
+    }
+  ];
+
+  hotProducts = [
+  {
+    rating: 4.5,
+    title: 'Black Stallion',
+    price: 120,
+    productImageUrl: 'assets/images/Hot/Black Stallion Roast.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.3,
+    title: 'Creamy Cloud',
+    price: 125,
+    productImageUrl: 'assets/images/Hot/Creamy Cloud Mocha.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.5,
+    title: 'White Wonder',
+    price: 135,
+    productImageUrl: 'assets/images/Hot/White Wonder Mocha.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.4,
+    title: 'Toffee Twist',
+    price: 100,
+    productImageUrl: 'assets/images/Hot/Toffee Twist Brew.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.5,
+    title: 'Caramel Delight',
+    price: 105,
+    productImageUrl: 'assets/images/Hot/Caramel Fudge Delight.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.7,
+    title: 'Black Noir',
+    price: 130,
+    productImageUrl: 'assets/images/Hot/Black Noir.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  }
+  ];
+
+  coldProducts = [
+  {
+    rating: 4.5,
+    title: 'Herbal Frost',
+    price: 120,
+    productImageUrl: 'assets/images/Cold/Herbal Frost.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.3,
+    title: 'Marble Ice',
+    price: 125,
+    productImageUrl: 'assets/images/Cold/Marble Ice.PNG',
+    favorite: false,
+    productId: 124,
+    productPath: '/product-details/124'
+  },
+  {
+    rating: 4.5,
+    title: 'Vanilla Bean Brew',
+    price: 135,
+    productImageUrl: 'assets/images/Cold/Vanilla Bean Brew.PNG',
+    favorite: false,
+    productId: 125,
+    productPath: '/product-details/125'
+  },
+  {
+    rating: 4.4,
+    title: 'Silk Brew',
+    price: 100,
+    productImageUrl: 'assets/images/Cold/Silk Brew.PNG',
+    favorite: false,
+    productId: 126,
+    productPath: '/product-details/126'
+  },
+  {
+    rating: 4.5,
+    title: 'Golden Cascad',
+    price: 105,
+    productImageUrl: 'assets/images/Cold/Golden Cascad.PNG',
+    favorite: false,
+    productId: 127,
+    productPath: '/product-details/127'
+  },
+  {
+    rating: 4.7,
+    title: 'Caramel Drift',
+    price: 130,
+    productImageUrl: 'assets/images/Cold/Caramel Drift.PNG',
+    favorite: false,
+    productId: 128,
+    productPath: '/product-details/128'
+  }
+  ];
+
+  nonCoffeeProducts = [
+  {
+    rating: 4.5,
+    title: 'Mango Wave',
+    price: 120,
+    productImageUrl: 'assets/images/Non-Coffee/Mango Cream Wave.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.3,
+    title: 'Fresh Berry Burst',
+    price: 125,
+    productImageUrl: 'assets/images/Non-Coffee/Fresh Berry Burst.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.5,
+    title: 'Berry Cool Milkshake',
+    price: 135,
+    productImageUrl: 'assets/images/Non-Coffee/Berry Cool Milkshake.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.4,
+    title: 'Mango Matcha',
+    price: 100,
+    productImageUrl: 'assets/images/Non-Coffee/Mango Matcha Delight.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.5,
+    title: 'Dragon Bliss',
+    price: 105,
+    productImageUrl: 'assets/images/Non-Coffee/Pink Dragon Bliss.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  },
+  {
+    rating: 4.7,
+    title: 'Cocomango Cooler',
+    price: 130,
+    productImageUrl: 'assets/images/Non-Coffee/Coco Mango Cooler.PNG',
+    favorite: false,
+    productId: 123,
+    productPath: '/product-details/123'
+  }
+  ];
+
+  pastryProducts = [
+    {
+      rating: 4.5,
+      title: 'BrewMille Feuille',
+      price: 120,
+      productImageUrl: 'assets/images/Pastry/BrewMille Feuille.PNG',
+      favorite: false,
+      productId: 123,
+      productPath: '/product-details/123'
+    },
+    {
+      rating: 4.3,
+      title: 'Berry Blast',
+      price: 125,
+      productImageUrl: 'assets/images/Pastry/Berry Cheesy Blast.PNG',
+      favorite: false,
+      productId: 123,
+      productPath: '/product-details/123'
+    },
+    {
+      rating: 4.5,
+      title: 'Raspberry Rewind',
+      price: 135,
+      productImageUrl: 'assets/images/Pastry/Raspberry Rewind.PNG',
+      favorite: false,
+      productId: 123,
+      productPath: '/product-details/123'
+    },
+    {
+      rating: 4.4,
+      title: 'Peachy Memory',
+      price: 100,
+      productImageUrl: 'assets/images/Pastry/Peachy Keen Memory.PNG',
+      favorite: false,
+      productId: 123,
+      productPath: '/product-details/123'
+    },
+    {
+      rating: 4.5,
+      title: 'CinnaDeja Delight',
+      price: 105,
+      productImageUrl: 'assets/images/Pastry/CinnaDeja Delight.PNG',
+      favorite: false,
+      productId: 123,
+      productPath: '/product-details/123'
+    },
+    {
+      rating: 4.7,
+      title: 'Choco Deja Puff',
+      price: 130,
+      productImageUrl: 'assets/images/Pastry/Choco Deja Puff.PNG',
+      favorite: false,
+      productId: 123,
+      productPath: '/product-details/123'
+    }
+  ];
+
+
+getSlideIndexes(products: any[], productsPerSlide: number) {
+  return Array.from({ length: Math.ceil(products.length / productsPerSlide) }, (_, i) => i * productsPerSlide);
+}
+
 
   constructor() {}
 
@@ -36,7 +309,7 @@ export class HomePage implements OnInit {
     }
   }
 
-    goToNotifications() {
+  goToNotifications() {
     window.location.href = '/notifications';
   }
   goToFavorites() {
